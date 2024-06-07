a6bd8652a108cd51bb3f7c0a3459ed1257c3f7b4
sandipghimire555 GITHUB <sandipghimire555@gmail.com>
2024-06-07 22:31:13 +0545

    Project setup
    Added domain for entity, Application for Services, Infrastrucure for repository, presentation for controller and web for outermost layer, Added Swagger, Fluent validation for validating input

---
+++ candidatehub.Application/DependencyResolution.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace candidatehub.Application
+{
+    public static class DependencyResolution
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = typeof(DependencyResolution).Assembly;
+            services.AddValidatorsFromAssembly(assembly);
+            return services;
+        }
+    }
+}
---
+++ candidatehub.Web/Program.cs
@@ -0,0 +1,30 @@
+using candidatehub.Application;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication().
+    AddPresentation().
+    AddInfrastructure();
+
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
