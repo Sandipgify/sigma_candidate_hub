abd464739363a967a7c19a24987caa99e5484177
sandipghimire555 GITHUB <sandipghimire555@gmail.com>
2024-06-08 00:04:33 +0545

    add serilog to log information

--- candidate_hub.presentation/DependencyResolution.cs
+++ candidate_hub.presentation/DependencyResolution.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using Serilog;
 
 namespace candidatehub.Application
 {
     public static class DependencyResolution
     {
         public static IServiceCollection AddPresentation(this IServiceCollection services)
-            {
+        {
             return services;
-            }
+        }
+
+        public static void ConfigureSerilog(this IServiceCollection services, IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+            services.AddLogging(logg=> logg.AddSerilog(Log.Logger));
+        }
     }
 }
